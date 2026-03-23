b9947e0530ccaabe6bfa0fac2324ff68b1d07806
Kisshor M <kisshormurali@gmail.com>
2026-03-24 00:26:36 +0530

    Added player entity

---
+++ src/main/java/com/example/lol/entity/Player.java
@@ -0,0 +1,81 @@
+package com.example.lol.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String department;
+    private String sport;
+    private String skillLevel;
+    private String contact;
+
+    public Player() {
+    }
+
+    public Player(Long id, String name, String department, String sport, String skillLevel, String contact) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.sport = sport;
+        this.skillLevel = skillLevel;
+        this.contact = contact;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(String skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
